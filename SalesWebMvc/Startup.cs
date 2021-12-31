e1a0ececc7f8d5e70f43ee4b99ae2b07e3436fbf
Anderson Lima <anderson.lima.araujo@hotmail.com>
2021-12-31 17:02:19 -0300

    number and date formatting

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -5,13 +5,14 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using System.Collections.Generic;
 
 namespace SalesWebMvc
 {
@@ -49,6 +50,16 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUs = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            { 
+                DefaultRequestCulture = new RequestCulture(enUs),
+                SupportedCultures = new List<CultureInfo> { enUs},
+                SupportedUICultures = new List<CultureInfo> { enUs},
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
